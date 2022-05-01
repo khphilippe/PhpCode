f1cd21734360616ac4613013ba17e5d0ee459518
Kévens Herbyson Philippe <kevensherbyson@gmail.com>
2022-05-01 13:38:54 -0300

    Segundo commit con asp file

--- FtpConnect.php
+++ FtpConnect.php
@@ -1,8 +1,5 @@
-
-
 <?php
-
-      $ftp_server = "";
+    $ftp_server = "";
       $conn_id = ftp_connect($ftp_server);
       $ftp_user_name = "";
       $ftp_user_pass = "";
@@ -10,23 +7,25 @@
       $contents = ftp_nlist($conn_id, '');
 
 
-	  function arrayfilter($elements, $query) {
+	  function arrayfilter($elements, $query)
+	  {
 		$found = [];
 		$query = strtolower($query);
-		foreach($elements as $element) {
+		foreach($elements as $element)
+		{
 		  $current = strtolower($element);
 		  if(strpos($current, $query) !== false) {
 			array_push($found, $element);
 		  }
 		}
 		return $found;
-	} 
+	  } 
 
-	// Listar los archivos filtrando  stock y price
-	$listaStock= arrayFilter($contents, 'stock');
-	$listaPrice= arrayFilter($contents, 'price');
+	 // Listar los archivos filtrando  stock y price
+	 $listaStock= arrayFilter($contents, 'stock');
+	 $listaPrice= arrayFilter($contents, 'price');
 
-	// Obtener el ultimo archivo con tal nombre(si es stock ultimo archivo stock)
+	 // Obtener el ultimo archivo con tal nombre(si es stock ultimo archivo stock)
 	 $lastElementStock = end($listaStock);
 	 $lastElementPrice = end($listaPrice);
 	 
@@ -34,33 +33,61 @@
 	 // Fijar hora del ultimo archivo
 	 $last_modStock = ftp_mdtm($conn_id, $lastElementStock);
 	 $last_modPrice = ftp_mdtm($conn_id, $lastElementPrice);
+
+
+	 function MostrarFecha($entero,$ultimoElemento) 
+	 {
+		if ($entero != -1) {
+		 
+			echo date("F d Y H:i:s.", $entero).". <br/> <br/>";
+		}
+		else {
+			echo "<br>could not get last modified.";
+		}
+	 }
+
+	 function BorrarRuta($archivoCompleto) 
+	 {
+	     echo"<a href=".substr($archivoCompleto,1).">".substr($archivoCompleto,24) .  "<br/> <br/> </a>";
+	 }
         
-	 if ($last_modStock != -1) {
-		 
-		 // Checking whether any error occurred or not
-		 // while retrieving last modified data
-		 echo "<br> $lastElementStock was modified on ".
-				 date("F d Y H:i:s.", $last_modStock).". <br/> <br/>";
-	 }
-	 else {
-		 echo "<br>could not get last modified.";
-	 }
-
-	 if ($last_modPrice != -1) {
-		 
-		// Checking whether any error occurred or not
-		// while retrieving last modified data
-		echo "<br> $lastElementPrice was modified on ".
-				date("F d Y H:i:s.", $last_modPrice).".";
-	}
-	else {
-		echo "<br>could not get last modified.";
-	}
-
-      ftp_close($conn_id);
-
-
+    ftp_close($conn_id);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head> 
+<body>
+
+<center>
+<table Border>
+
+  <tr>
+    <th scope="col">Nombre del proceso</th>
+    <th scope="col">Ultima fecha que corrio</th>
+  </tr>
+
+  <tr>
+    <td> <?php  BorrarRuta($lastElementStock); ?>
+    <td> <?php 	MostrarFecha($last_modStock,$lastElementStock); ?> </td>  
+  </tr>
+
+  <tr>
+    <td><?php  BorrarRuta($lastElementPrice); ?></td>
+    <td> <?php MostrarFecha($last_modPrice,$lastElementPrice); ?> </td>
+  </tr>
+</table>
+	</center>
+
+</body>
+</html>
+
+
 
 
 
---
+++ primer.asp
@@ -0,0 +1,23 @@
+<%
+  'response.write("good")
+  'response.Redirect("http://localhost:8080/ejercicios/FtpConnect.php")
+ 
+%>
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=8.0">
+	<iframe frameborder=0 width=100% height=100% scr="http://localhost:8080/ejercicios/FtpConnect.php" a src="http://localhost:8080/ejercicios/FtpConnect.php"> </iframe>
+	<style type="text/css">
+		html, body, div, iframe { margin:0; padding:0; height:100%; }
+		iframe { display:block; width:100%; border:none; }
+	</style>
+	</head> 
+  <body>
+
+</body>
+</html>
